648eda053619b45f030311e4ea640a3502d63a0b
Mindras Eugen Traian <mandras_eugen@yahoo.com>
2026-07-01 19:44:54 +0200

    mod: created file index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+export * from "./cambia-colore/js/colorChanger.js";
+export * from "./numero-casuale/js/randomNumber.js";
+export * from "./selettore-rapido/js/shortcut.js";
+export * from "./crea-elemento/js/createElement.js";
+export * from "./on-listener/js/eventListener.js";
+export * from "./log-color/js/colorLogger.js";
+export * from "./fade-in/js/animations.js";
