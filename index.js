6f7c14bae469ff667ce8aeb57508b9636dd87ff8
Mindras Eugen Traian <mandras_eugen@yahoo.com>
2026-07-03 16:14:27 +0200

    docs: add documentation comments explaining the library entry point structure in index.js

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ export * from "./crea-elemento/js/createElement.js";
 export * from "./on-listener/js/eventListener.js";
 export * from "./log-color/js/colorLogger.js";
 export * from "./fade-in/js/animations.js";
+
+/* questo file serve come punto di ingresso principale e serve per exportare tutte le funzioni del progetto in un unica "libreria"
+ funzionamento:
+ - importa tutti i moduli dalle varie cartelle 
+ -li riesporta tutti insieme
+Questo serve per poter usare tutte le funzioni del progetto con un solo import:
+
+import * as utils from "./index.js";
+utils.cambiaColore(document.body);
+
+
+ */
